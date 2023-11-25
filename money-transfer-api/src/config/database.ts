5c7ec01907046e4af5bbcf841380ac24b7074f82
Decagon <chizobaebuka1@gmail.com>
2023-11-25 02:41:41 +0100

    feat: added the correct mongoUrl and fixed the createUser endpoint

--- money-transfer-api/src/config/database.ts
+++ money-transfer-api/src/config/database.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 export const connection = async () => {
     try {
-        const db = mongoose.connect(`mongodb+srv://group5:qwertyuiop@empmanage.bdbqemp.mongodb.net/`)
+        const db = mongoose.connect(`mongodb+srv://martinfresh8:qwertyuiop!23@cluster0.fze7kqn.mongodb.net/`)
         console.log(`Mongodb connected`)
     } catch (err) {
         console.log(err)
